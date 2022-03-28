d7ab47f665e2bde93ffc7b26198d2fd0eabd5136
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-03-28 17:02:08 +0300

    Add handlers module for assistant service package

---
+++ services/assistant/handlers.py
@@ -0,0 +1,23 @@
+from aiotdlib import Client
+from aiotdlib.api import UpdateNewMessage
+from loguru import logger
+
+from utils.youtube import extract_link
+
+
+async def handle_new_own_message(client: Client, update: UpdateNewMessage):
+    own_id = await client.get_my_id()
+    chat_id = update.message.chat_id
+    sender_id = update.message.sender_id.user_id
+
+    # handle only own messages that was sent anyone
+    if own_id != sender_id:
+        return
+
+    msg = update.message.content.text.text
+    link = extract_link(msg)
+    if not link:
+        return
+
+    download_and_send_youtube_video.delay(chat_id, link)
+    logger.info(f'downloading link: {link}')
