dd33c5d573c68afaa58499df5ed490ff0bcf9cb4
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-03-30 13:16:53 +0300

    Replace MY_TELEGRAM_ID to assistant manager config module

--- app/config.py
+++ app/config.py
@@ -6,5 +6,3 @@ import os
 
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = os.environ.get('REDIS_PORT')
-
-MY_TELEGRAM_ID = int(os.environ.get('MY_TELEGRAM_ID'))
--- services/assistant_manager/config.py
+++ services/assistant_manager/config.py
@@ -5,3 +5,4 @@ import os
 
 # Telegram
 TELEGRAM_API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
+MY_TELEGRAM_ID = int(os.environ.get('MY_TELEGRAM_ID'))
