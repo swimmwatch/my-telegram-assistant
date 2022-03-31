030eeecbba547d6dcd38f4bb1c06d93ff749de9d
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-01 01:32:23 +0300

    Add ASSISTANT_SERVICE_ADDR env variable in worker config module

--- services/worker/config.py
+++ services/worker/config.py
@@ -5,3 +5,6 @@ import os
 
 BACKEND_URL = os.environ.get('BACKEND_URL')
 BROKER_URL = os.environ.get('BROKER_URL')
+
+# Assistant gRPC service
+ASSISTANT_SERVICE_ADDR = os.environ.get('ASSISTANT_SERVICE_ADDR')
