eb610db8ffc7805f7fa4aa85074df697953cb2ca
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-01 02:47:39 +0300

    Fix returning type of send_text

--- services/assistant/__init__.py
+++ services/assistant/__init__.py
@@ -2,8 +2,8 @@
 Assistant service package.
 """
 import aiotdlib
+from google.protobuf.empty_pb2 import Empty
 
-from services.assistant.assistant_pb2 import SendTextRequest, SendVideoRequest
 from services.assistant.assistant_pb2_grpc import AssistantServicer
 
 
@@ -13,13 +13,14 @@ class AsyncAssistantService(AssistantServicer):
 
         self.aiotdlib_client = aiotdlib_client
 
-    async def send_text(self, request: SendTextRequest, context) -> None:
+    async def send_text(self, request, context) -> Empty:
         # TODO: handle errors from Aiotdlib
         await self.aiotdlib_client.send_text(
             request.chat_id,
             request.text,
             disable_notification=request.disable_notification
         )
+        return Empty()
 
-    async def send_video(self, request: SendVideoRequest, context):
+    async def send_video(self, request, context):
         raise NotImplementedError
