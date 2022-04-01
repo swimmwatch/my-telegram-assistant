3952159175e3ed5c01056bd9ddf2df25f1f3f3c2
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-01 16:12:39 +0300

    Set default value for MY_TELEGRAM_ID

--- services/assistant_manager/config.py
+++ services/assistant_manager/config.py
@@ -5,4 +5,4 @@ import os
 
 # Telegram
 TELEGRAM_API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
-MY_TELEGRAM_ID = int(os.environ.get('MY_TELEGRAM_ID'))
+MY_TELEGRAM_ID = int(os.environ.get('MY_TELEGRAM_ID', 0))
