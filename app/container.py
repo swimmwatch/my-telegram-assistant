e4c9d565b5ae78b3dbbb68f168cb22d6ae4d9835
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-01 17:00:31 +0300

    Remove from main DI container YouTubeScraperService

--- app/container.py
+++ app/container.py
@@ -6,7 +6,6 @@ from dependency_injector.wiring import providers
 from redis import Redis
 
 from app.config import REDIS_HOST, REDIS_PORT
-from services.youtube_scraper import YouTubeScraperService
 
 
 class Container(DeclarativeContainer):
@@ -14,7 +13,4 @@ class Container(DeclarativeContainer):
         Redis,
         host=REDIS_HOST,
         port=REDIS_PORT
-    )
-    youtube_scraper_service = providers.Singleton(
-        YouTubeScraperService
     )
