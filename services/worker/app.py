ec46d5f6794ed9200986597c6921f3052bb200b6
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-01 17:04:09 +0300

    Fix env variable ASSISTANT_GRPC_ADDR

--- services/worker/app.py
+++ services/worker/app.py
@@ -4,12 +4,14 @@ Init Celery application.
 import os
 
 from celery import Celery
-from dependency_injector.wiring import inject, Provide
+from dependency_injector.wiring import inject
 from pytube import YouTube
 
-from services.assistant.assistant_pb2 import SendTextRequest
+from services.assistant.assistant_pb2 import SendVideoRequest
 from services.assistant.grpc_client import AssistantGrpcClient
+# TODO: use env variables for init Celery broker and backend
 # from services.worker.config import BROKER_URL, BACKEND_URL
+from services.worker.config import ASSISTANT_GRPC_ADDR
 from services.worker.container import WorkerContainer
 
 celery = Celery(broker='redis://redis', backend='redis://redis')
@@ -26,7 +28,8 @@ def init_di_container(sender, **kwargs):
 def download_and_send_youtube_video(
         chat_id: int,
         link: str,
-        assistant_grpc_client: AssistantGrpcClient = Provide[WorkerContainer.assistant_grpc_client]
+        # TODO: fix it
+        # assistant_grpc_client: AssistantGrpcClient = Provide[WorkerContainer.assistant_grpc_client]
 ):
     yt = YouTube(link)
     stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
@@ -35,5 +38,6 @@ def download_and_send_youtube_video(
     stream.download(out_dir, out_filename)
     video_path = os.path.join(out_dir, out_filename)
 
-    req = SendTextRequest(chat_id=chat_id, text=video_path, disable_notification=True)
-    assistant_grpc_client.stub.send_text(req)
+    assistant_grpc_client = AssistantGrpcClient(ASSISTANT_GRPC_ADDR)
+    req = SendVideoRequest(chat_id=chat_id, video_path=video_path)
+    assistant_grpc_client.stub.send_video(req)
--- services/worker/container.py
+++ services/worker/container.py
@@ -5,11 +5,11 @@ from dependency_injector.containers import DeclarativeContainer
 from dependency_injector.wiring import providers
 
 from services.assistant.grpc_client import AssistantGrpcClient
-from services.worker.config import ASSISTANT_SERVICE_ADDR
+from services.worker.config import ASSISTANT_GRPC_ADDR
 
 
 class WorkerContainer(DeclarativeContainer):
     assistant_grpc_client = providers.Factory(
         AssistantGrpcClient,
-        addr=f'{ASSISTANT_SERVICE_ADDR}'
+        addr=f'{ASSISTANT_GRPC_ADDR}'
     )
