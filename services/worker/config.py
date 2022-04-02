dedbba6ed6f3fa79f676cbf2c101128065013ace
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-02 11:59:02 +0300

    Add YT_VIDEO_TTL constant to worker config module

--- services/worker/config.py
+++ services/worker/config.py
@@ -12,3 +12,4 @@ ASSISTANT_GRPC_ADDR = os.environ.get('ASSISTANT_GRPC_ADDR', 'localhost')
 # YouTube
 YT_MAX_VIDEO_LENGTH = 60 * 3
 YT_OUT_DIR = os.path.join('/tmp', 'youtube')
+YT_VIDEO_TTL = 60 * 2
