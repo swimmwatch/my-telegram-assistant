2ea1202048b414c8259ffb52a6b52e646afc74b3
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-14 19:59:41 +0300

    Add to worker container post_cache_state_manager and sent_post_msg_info_cache_manager

--- services/worker/container.py
+++ services/worker/container.py
@@ -6,7 +6,9 @@ from dependency_injector.wiring import providers
 from redis import Redis
 
 from services.assistant.grpc_client import AssistantGrpcClient
+from services.sent_post_msg_info_cache_manager import SentPostMessageInfoCacheManager
 from services.worker.config import ASSISTANT_GRPC_ADDR, REDIS_HOST, REDIS_PORT
+from utils.post.cache.state.redis import RedisPostStateCacheManager
 
 
 class WorkerContainer(DeclarativeContainer):
@@ -18,4 +20,12 @@ class WorkerContainer(DeclarativeContainer):
         Redis,
         host=REDIS_HOST,
         port=REDIS_PORT
+    )
+    post_cache_state_manager = providers.Singleton(
+        RedisPostStateCacheManager,
+        redis_client=redis_client
+    )
+    sent_post_msg_info_cache_manager = providers.Singleton(
+        SentPostMessageInfoCacheManager,
+        redis_client=redis_client
     )
