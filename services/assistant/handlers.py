fdca7c9578b8085a7de03f2317caf67f92512a3d
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-16 19:55:04 +0300

    Handle removing web page preview

--- services/assistant/handlers.py
+++ services/assistant/handlers.py
@@ -8,7 +8,7 @@ from utils.youtube import extract_link
 
 
 @serve_only_own_actions
-async def handle_new_own_message(_: Client, update: UpdateNewMessage):
+async def handle_new_own_message(client: Client, update: UpdateNewMessage):
     chat_id = update.message.chat_id
 
     content = update.message.content
@@ -20,6 +20,9 @@ async def handle_new_own_message(_: Client, update: UpdateNewMessage):
     link = extract_link(msg)
     if not link:
         return
+
+    # remove web page preview
+    await client.edit_text(chat_id, update.message.id, text=msg, disable_web_page_preview=True)
 
     download_and_send_post.delay(chat_id, link)
     logger.info(f'downloading post: {link}')
