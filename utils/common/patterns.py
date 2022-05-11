9968bbba292dd05ec20322ab0a3f973eeb597eb6
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-05-11 12:41:53 +0300

    Add Chain Of Responsibility pattern

--- utils/common/patterns.py
+++ utils/common/patterns.py
@@ -1,6 +1,7 @@
 """
 Common patterns.
 """
+from abc import ABC, abstractmethod
 from typing import Protocol
 
 
@@ -14,3 +15,19 @@ class Repository(Protocol):
 class Factory(Protocol):
     """Factory protocol."""
     pass
+
+
+class ChainOfResponsibility(ABC):
+    """Chain Of Responsibility pattern"""
+    def __init__(self, next_handler: 'ChainOfResponsibility'):
+        self._next_handler = next_handler
+
+    def handle(self, request):
+        handled = self.process_request(request)
+
+        if not handled:
+            self._next_handler.handle(request)
+
+    @abstractmethod
+    def process_request(self, request) -> bool:
+        pass
