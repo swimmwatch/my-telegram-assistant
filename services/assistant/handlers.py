88fd72852780a47383f8b4c03d6b4cb44d51eb6f
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-05-11 13:01:07 +0300

    Refactor handler of own messages

--- services/assistant/handlers.py
+++ services/assistant/handlers.py
@@ -1,10 +1,12 @@
 from aiotdlib import Client
 from aiotdlib.api import UpdateNewMessage, MessageText
-from loguru import logger
 
-from services.worker.app import download_and_send_post
+from services.assistant.commands import YouTubeShortVideoDownloadCommandHandler, TikTokVideoDownloadCommandHandler, \
+    CommandRequest
 from utils.aiotdlib.decorators import serve_only_own_actions
-from utils.youtube import extract_link
+
+
+commands = YouTubeShortVideoDownloadCommandHandler(TikTokVideoDownloadCommandHandler(None))
 
 
 @serve_only_own_actions
@@ -17,9 +19,6 @@ async def handle_new_own_message(client: Client, update: UpdateNewMessage):
 
     formatted_text = content.text
     msg = formatted_text.text
-    link = extract_link(msg)
-    if not link:
-        return
 
     # remove web page preview
     await client.edit_text(
@@ -29,5 +28,5 @@ async def handle_new_own_message(client: Client, update: UpdateNewMessage):
         disable_web_page_preview=True
     )
 
-    download_and_send_post.delay(chat_id, link)
-    logger.info(f'downloading post: {link}')
+    command_request = CommandRequest(message=msg, chat_id=chat_id)
+    commands.handle(command_request)
