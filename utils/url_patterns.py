34aab7cb8289e9b112ca3a4073a976dc8816e95e
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-05-11 14:41:12 +0300

    Add URL RegExp pattern for TikTok

--- utils/url_patterns.py
+++ utils/url_patterns.py
@@ -7,3 +7,16 @@ YOUTUBE_LINK_PATTERN = re.compile(
     r'((?:https?:)?\/\/)?((?:www|m)\.)?'
     r'((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?'
 )
+
+TIKTOK_LINK_PATTERN = re.compile(
+    r'(?x)'
+    r'https?://'
+    r'(?:'
+    r'(?:www|m|vt)\.'
+    r'(?:tiktok\.com)\/'
+    r'(?:.+)'
+    r'(?:\/)?'
+    r'(?:\?.+=)?'
+    r')'
+    r'(?P<id>[\da-z]+)'
+)
