92c3d3f4fa6e0054ee46a8aae1a6c069d2db32bb
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-01 03:45:27 +0300

    Extend hello command

--- services/assistant/handlers/hello/__init__.py
+++ services/assistant/handlers/hello/__init__.py
@@ -11,8 +11,12 @@ hello_command = ExplicitCommand(name="hello")
 @hello_command.on()
 async def handle_welcome_output(_: ParsedArguments, request: CommandRequest):
     project_link_html = a({'href': 'https://github.com/swimmwatch/my-telegram-assistant'}, 'Telegram Assistant')
+    user_info = await request.client.get_user(request.message.chat_id)
+    username = user_info.username
+    if not username:
+        username = f'{user_info.first_name} {user_info.last_name}'
     res_message = '\n'.join([
-        f"Hi! This message was sent by \N{ROBOT FACE} {project_link_html}.",
+        f"Hi, {username}! This message was sent by \N{ROBOT FACE} {project_link_html}.",
         "In short, this program helps to automate messaging in Telegram."
     ])
     await request.client.send_text(
