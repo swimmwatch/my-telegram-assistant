a8430a61b82f1d81397d0f0798c6c1baf70ced14
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-08-12 00:44:45 +0300

    Remove phone_number field

--- services/assistant/config.py
+++ services/assistant/config.py
@@ -8,7 +8,6 @@ from pydantic import BaseSettings
 class AssistantSettings(BaseSettings):
     aiotdlib_api_id: int = 0
     aiotdlib_api_hash: str = ''
-    phone_number: str = ''
     assistant_grpc_addr: str = 'localhost:50051'
 
     assistant_qr_login_timeout: float = 60.0
