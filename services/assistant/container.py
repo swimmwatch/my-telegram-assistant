1286658bb971051c1ea739bc8adb78ae1d7f3b35
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-08-12 13:23:53 +0300

    Fix mypy issue with type annotation

--- services/assistant/container.py
+++ services/assistant/container.py
@@ -2,7 +2,7 @@
 Assistant DI container.
 """
 from dependency_injector.containers import DeclarativeContainer
-from dependency_injector.wiring import providers
+from dependency_injector.providers import Singleton, Factory
 from telethon import TelegramClient
 
 from services.assistant.assistant import Assistant
@@ -13,22 +13,22 @@ from services.assistant_manager.grpc.client import AssistantManagerGrpcClient
 
 
 class AssistantContainer(DeclarativeContainer):
-    telegram_client = providers.Singleton(
+    telegram_client: Singleton[TelegramClient] = Singleton(
         TelegramClient,
         session='anon',
         api_id=assistant_settings.aiotdlib_api_id,
         api_hash=assistant_settings.aiotdlib_api_hash,
     )
-    assistant_manager_grpc_client = providers.Factory(
+    assistant_manager_grpc_client = Factory(
         AssistantManagerGrpcClient,
         addr=assistant_manager_settings.assistant_manager_grpc_addr
     )
-    assistant = providers.Singleton(
+    assistant = Singleton(
         Assistant,
         telegram_client=telegram_client.provided,
         assistant_manager_grpc_client=assistant_manager_grpc_client.provided
     )
-    assistant_entrypoint = providers.Factory(
+    assistant_entrypoint = Factory(
         AssistantEntrypoint,
         assistant=assistant.provided
     )
