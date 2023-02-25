0fdf94bd50afdb90b7f9d3d538cc60a621ffd9f1
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-02-25 11:20:02 +0300

    Add TELEGRAM_BOT_WEBAPP_URL variable

--- src/services/assistant_manager/config.py
+++ src/services/assistant_manager/config.py
@@ -1,11 +1,12 @@
 """
 Assistant manager configuration.
 """
-from pydantic import BaseSettings, SecretStr
+from pydantic import BaseSettings, SecretStr, AnyHttpUrl
 
 
 class AssistantManagerSettings(BaseSettings):
     my_telegram_id: int = 0
     telegram_api_token: SecretStr
+    telegram_bot_webapp_url: AnyHttpUrl
 
     assistant_manager_grpc_addr: str
