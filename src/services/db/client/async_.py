5560eaf5841255c98cb06d69d5dc4cb186f82dae
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-03-11 11:42:07 +0300

    Refactor database DAL

--- src/services/db/client/async_.py
+++ src/services/db/client/async_.py
@@ -5,6 +5,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 
 from ..config import DatabaseSettings
 
-database_settings = DatabaseSettings()
+database_settings = DatabaseSettings()  # type: ignore
 async_engine = create_async_engine(database_settings.db_url, echo=database_settings.debug)
 AsyncSession_ = async_sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
--- src/services/db/client/sync.py
+++ src/services/db/client/sync.py
@@ -6,6 +6,6 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from services.db.config import DatabaseSettings
 
-database_settings = DatabaseSettings()
+database_settings = DatabaseSettings()  # type: ignore
 engine = create_engine(database_settings.db_url, echo=database_settings.debug)
 Session_ = scoped_session(sessionmaker(bind=engine))
