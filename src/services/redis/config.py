023104647f2d737d2744e8c86842625e41d48fad
dm.vasilev <dm.vasilev@rambler-co.ru>
2023-06-20 16:08:10 +0300

    refactor: update Redis configuration

--- src/services/redis/config.py
+++ src/services/redis/config.py
@@ -3,10 +3,16 @@ Redis service configuration.
 """
 from pydantic import BaseSettings
 
+from services.common.config import RunLevelBaseConfigMixin
 
-class RedisSettings(BaseSettings):
-    host: str = "localhost"
-    port: int = 6364
+
+class RedisSettings(RunLevelBaseConfigMixin, BaseSettings):
+    host: str = "localhost:6380"
+    db: int = 0
+
+    @property
+    def url(self):
+        return f"redis://{self.host}/{self.db}"
 
     class Config:
         env_prefix = "redis_"
