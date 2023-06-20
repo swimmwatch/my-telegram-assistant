41da09bd3fdae4d8c3e6467b94534106e0ddd9e1
dm.vasilev <dm.vasilev@rambler-co.ru>
2023-06-20 16:24:17 +0300

    refactor: remove types.py

--- src/services/db/types.py
+++
@@ -1,9 +0,0 @@
-"""
-Custom typings for DB package.
-"""
-from typing import TypeVar
-
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session
-
-SessionType = TypeVar("SessionType", AsyncSession, Session)
