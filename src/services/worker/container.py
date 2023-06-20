54a80f8732184c05d3484b7efd2dce60654b06e2
dm.vasilev <dm.vasilev@rambler-co.ru>
2023-06-20 16:36:35 +0300

    fix: resolve mypy issue

--- src/services/worker/container.py
+++ src/services/worker/container.py
@@ -2,7 +2,8 @@
 Worker DI container.
 """
 from dependency_injector.containers import DeclarativeContainer
-from dependency_injector.providers import Factory, Singleton
+from dependency_injector.providers import Factory
+from dependency_injector.providers import Singleton
 from redis import Redis
 
 from services.assistant.config import AssistantSettings
@@ -18,5 +19,5 @@ assistant_settings = AssistantSettings()  # type: ignore
 
 class WorkerContainer(DeclarativeContainer):
     assistant_grpc_client = Factory(AssistantGrpcClient, addr=assistant_settings.assistant_grpc_addr)
-    redis_client = Singleton(Redis, host=redis_settings.host, port=redis_settings.port)
+    redis_client = Singleton(Redis, host=redis_settings.host)
     post_cache_state_manager = Singleton(RedisPostStateCacheManager, redis_client=redis_client)
