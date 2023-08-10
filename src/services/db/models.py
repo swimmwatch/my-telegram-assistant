5dac931464353727414b9256186c85f2a6fb0037
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-08-10 12:41:49 +0300

    feat: add user model

--- src/services/db/models.py
+++ src/services/db/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import String
+import sqlalchemy as sa
+from sqlalchemy import orm
 
 from services.db.base import Base
 
@@ -8,5 +7,6 @@ from services.db.base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    id = orm.mapped_column(sa.Integer, primary_key=True)
+    tg_id = orm.mapped_column(sa.Integer, primary_key=True, unique=True, index=True)
+    session = orm.mapped_column(sa.String, nullable=True, default=None)
