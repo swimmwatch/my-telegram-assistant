5dd36f87d85288619d0343e92aabcd88e9449eb1
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-08-11 12:21:50 +0300

    feat: add async types for session

--- src/utils/sqlalchemy/types.py
+++ src/utils/sqlalchemy/types.py
@@ -4,6 +4,9 @@ SQLAlchemy typing utilities.
 import typing
 
 from sqlalchemy import orm
+from sqlalchemy.ext.asyncio import AsyncSession
 
 SessionFactory = typing.Callable[..., typing.ContextManager[orm.Session]]
+AsyncSessionFactory = typing.Callable[..., typing.AsyncContextManager[AsyncSession]]
 SessionGenerator = typing.Generator[orm.Session, None, None]
+AsyncSessionGenerator = typing.AsyncGenerator[AsyncSession, None]
