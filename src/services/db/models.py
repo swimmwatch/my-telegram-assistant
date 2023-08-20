cbf312abf2718eafe48d85f899a1f9529acaf1b7
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-08-20 14:54:05 +0300

    feat: add "created", "updated" fields for user model

--- src/services/db/models.py
+++ src/services/db/models.py
@@ -10,3 +10,13 @@ class User(Base):
     id = orm.mapped_column(sa.Integer, primary_key=True)
     tg_id = orm.mapped_column(sa.Integer, unique=True, index=True)
     session = orm.mapped_column(sa.String, nullable=True, default=None)
+    created = orm.mapped_column(
+        sa.DateTime(timezone=True),
+        nullable=False,
+        server_default=sa.func.now(),
+    )
+    updated = orm.mapped_column(
+        sa.DateTime(timezone=True),
+        nullable=True,
+        server_onupdate=sa.func.now(),
+    )
