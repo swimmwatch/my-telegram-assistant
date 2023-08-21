13cfdc713993c8289a05469133e164f452656ece
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-08-21 17:20:18 +0300

    refactor: simplify gRPC client declaration

--- src/services/assistant/grpc_/client.py
+++ src/services/assistant/grpc_/client.py
@@ -2,9 +2,15 @@
 gRPC Assistant client.
 """
 from services.assistant.assistant_pb2_grpc import AssistantStub
+from utils.grpc.client import AsyncGrpcClient
 from utils.grpc.client import GrpcClient
 
 
 class AssistantGrpcClient(GrpcClient):
-    def __init__(self, addr: str):
-        super().__init__(addr, AssistantStub)
+    class Meta:
+        stub = AssistantStub
+
+
+class AssistantAsyncGrpcClient(AsyncGrpcClient):
+    class Meta:
+        stub = AssistantStub
--- src/services/assistant_manager/grpc_/client.py
+++ src/services/assistant_manager/grpc_/client.py
@@ -2,9 +2,15 @@
 Assistant manager gPRC client.
 """
 from services.assistant_manager.assistant_manager_pb2_grpc import AssistantManagerStub
+from utils.grpc.client import AsyncGrpcClient
 from utils.grpc.client import GrpcClient
 
 
 class AssistantManagerGrpcClient(GrpcClient):
-    def __init__(self, addr: str):
-        super().__init__(addr, AssistantManagerStub)
+    class Meta:
+        stub = AssistantManagerStub
+
+
+class AssistantManagerAsyncGrpcClient(AsyncGrpcClient):
+    class Meta:
+        stub = AssistantManagerStub
