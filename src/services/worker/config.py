c3909e4f36c61408ceb56949b56bc6a9c88dfcb4
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-08-29 22:17:18 +0300

    refactor: remove unused variables

--- src/services/worker/config.py
+++ src/services/worker/config.py
@@ -1,5 +1,5 @@
 """
-Celery worker configuration.
+Worker configuration.
 """
 import os
 
@@ -9,9 +9,7 @@ from pydantic import RedisDsn
 from services.common.config import BaseConfig
 
 # YouTube
-YT_MAX_VIDEO_LENGTH = 60 * 3
 OUT_DIR = os.path.join("/tmp", "posts")  # noqa
-YT_VIDEO_TTL = 60 * 2
 
 
 class WorkerSettings(BaseSettings):
