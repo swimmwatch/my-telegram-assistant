1c11bd54d4f69907dbb2feb76edce52b2c1a18a3
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-08-29 14:49:55 +0300

    refactor: refactoring Worker DI container settings

--- src/services/worker/container.py
+++ src/services/worker/container.py
@@ -1,9 +1,8 @@
 """
 Worker DI container.
 """
+from dependency_injector import providers
 from dependency_injector.containers import DeclarativeContainer
-from dependency_injector.providers import Factory
-from dependency_injector.providers import Singleton
 from redis import Redis
 
 from services.assistant.config import AssistantSettings
@@ -12,12 +11,21 @@ from services.post.cache.redis import RedisPostStateCache
 from services.redis.config import RedisSettings
 from services.worker.config import WorkerSettings
 
-worker_settings = WorkerSettings()  # type: ignore
-redis_settings = RedisSettings()  # type: ignore
-assistant_settings = AssistantSettings()  # type: ignore
-
 
 class WorkerContainer(DeclarativeContainer):
-    assistant_grpc_client = Factory(AssistantGrpcClient, addr=assistant_settings.assistant_grpc_addr)
-    redis_client = Singleton(Redis.from_url, url=redis_settings.url)
-    post_cache_state: Singleton[RedisPostStateCache] = Singleton(RedisPostStateCache, redis_client=redis_client)
+    worker_settings = WorkerSettings()
+    redis_settings = RedisSettings()
+    assistant_settings = AssistantSettings()
+
+    assistant_grpc_client = providers.Factory(
+        AssistantGrpcClient,
+        addr=assistant_settings.grpc_addr,
+    )
+    redis_client = providers.Singleton(
+        Redis.from_url,
+        url=redis_settings.url,
+    )
+    post_cache_state: providers.Singleton[RedisPostStateCache] = providers.Singleton(
+        RedisPostStateCache,
+        redis_client=redis_client,
+    )
