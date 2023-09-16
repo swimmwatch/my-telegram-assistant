9bac44782c410de26d7edd0616615a01c37d12b6
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2023-09-16 22:26:03 +0300

    refactor: use "TimedMixin"

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -3,20 +3,12 @@ from sqlalchemy import orm
 
 from infrastructure.db.base import Base
 
+from .utils.mixins import TimedMixin
 
-class User(Base):
+
+class User(TimedMixin, Base):
     __tablename__ = "users"
 
     id = orm.mapped_column(sa.Integer, primary_key=True)
     tg_id = orm.mapped_column(sa.Integer, unique=True, index=True)
     session = orm.mapped_column(sa.String, nullable=True, default=None)
-    created = orm.mapped_column(
-        sa.DateTime(timezone=True),
-        nullable=False,
-        server_default=sa.func.now(),
-    )
-    updated = orm.mapped_column(
-        sa.DateTime(timezone=True),
-        nullable=True,
-        server_onupdate=sa.func.now(),  # type: ignore
-    )
